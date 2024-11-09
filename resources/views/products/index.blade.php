862336ee0beca166538a1ade2410a163e036508d
Mostakimul Karim <mostakimulkarim@gmail.com>
2024-11-09 04:56:29 -0500

    [feat]: product delete successfully

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -25,7 +25,7 @@
                         <td>
                             <a href="{{ route('products.show', $product->id) }}" class="btn btn-info btn-sm">View</a>
                             <a href="{{ route('products.edit', $product->id) }}" class="btn btn-warning btn-sm">Edit</a>
-                            <form action="#" method="POST" style="display:inline;">
+                            <form action="{{ route('products.destroy', $product->id) }}" method="POST" style="display:inline;">
                                 @csrf
                                 @method('DELETE')
                                 <button type="submit" class="btn btn-danger btn-sm">Delete</button>
